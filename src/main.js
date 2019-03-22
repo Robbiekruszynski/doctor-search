54d3356d6f2f3c52fd0280f0d9c33fde2b008b02
Student <example@example.com>
2019-03-22 11:58:36 -0700

    add logic / promise then for name search

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ import { grabApi } from "../src/api.js";
 $(document).ready(function() {
   $(".callDoctor").click(function() {
     let newApi = new grabApi();
-    let promise = newApi.runDoctor();
     let input = $("#issue").val();
-    let nameInput = $("#name").val();
+    // let nameInput = $("#name").val();
+    let promise = newApi.runDoctor();
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      const newObject = body.data;
+      // const newObject = body.data;
       console.log(body);
       body.data.forEach(function(index){
         for (let i = 0; i < index.specialties.length; i++) {
@@ -35,12 +35,41 @@ $(document).ready(function() {
            $(".printInfo").append("<img src='" + pic + "'>");
 
            break;
+
+         } else {
+           $(".printInfo").append("<p>Sorry, there is no current data or Doctor for that search</p>");
          }
        }
      });
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     })
+  });
 
+  $(".callName").click(function() {
+    let newApi = new grabApi();
+    let promise = newApi.runDoctor();
+    let nameInput = $("#name").val();
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      body.data.forEach(function(index){
+        if(index.profile.bio.match(nameInput)){
+          let firstName = index.profile.first_name;
+          $(".printInfo").append("<p> First Name: " + firstName + "</p>");
+
+          let lastName = index.profile.last_name;
+          $(".printInfo").append("<p> Last Name: " + lastName + "</p>");
+
+          let bio = index.profile.bio;
+          $(".printInfo").append("<p> Bio: " + bio + "</p>");
+
+          let gender = index.profile.gender;
+          $(".printInfo").append("<p> gender: " + gender + "</p>");
+
+          let pic = index.profile.image_url;
+          $(".printInfo").append("<img src='" + pic + "'>");
+        }
+      })
+    });
   });
 });
