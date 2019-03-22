8ddc0c3e8b06d80136d02f90367fcdba90dccbb9
Student <example@example.com>
2019-03-22 10:50:38 -0700

    add .env logic that works to hide API key, reformat webpack

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ export class grabApi {
   runDoctor() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const apiKey = process.env.exports.apiKey;
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=41.8781,-87.6298,100&skip=2&limit=10&user_key=' + apiKey;
+      let apiKey = process.env.exports.apiKey;
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=45.5122,-122.6587,100&skip=2&limit=10&user_key=' + apiKey;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import './sass/styles.scss';
 import { grabApi } from "../src/api.js";
 
 $(document).ready(function() {
-  $(".callOnApi").click(function() {
+  $(".callDoctor").click(function() {
     let newApi = new grabApi();
     let promise = newApi.runDoctor();
 
@@ -13,14 +13,27 @@ $(document).ready(function() {
       let body = JSON.parse(response);
       const newObject = body.data;
       console.log(body);
-      let input = $("#").val();
+      let input = $("#issue").val();
+      let nameInput = $("#name").val();
       body.data.forEach(function(index){
         for (let i = 0; i < index.specialties.length; i++) {
          if (index.specialties[i].description.match(input)) {
+
+           let firstName = index.profile.first_name;
+           $(".printInfo").append("<p> First Name: " + firstName + "</p>");
+
+           let lastName = index.profile.last_name;
+           $(".printInfo").append("<p> Last Name: " + lastName + "</p>");
+
            let bio = index.profile.bio;
-           $(".printInfo").append("<p>" + bio + "</p>");
+           $(".printInfo").append("<p> Bio: " + bio + "</p>");
+
+           let gender = index.profile.gender;
+           $(".printInfo").append("<p> gender: " + gender + "</p>");
+
            let pic = index.profile.image_url;
            $(".printInfo").append("<img src='" + pic + "'>");
+
            break;
          }
        }
